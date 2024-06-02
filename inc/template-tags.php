ca809acc659a045cde402f86a4e812b11a1891b0
Emanoel <victoremanoel23@gmail.com>
2024-06-02 17:12:21 +0200

    Reshape meta info on archive page's posts

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -23,7 +23,7 @@ if ( ! function_exists( 'understrap_posted_on' ) ) {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s"> (%4$s) </time>';
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s"> • Last updated on %4$s </time>';
 		}
 
 		$time_string = sprintf(
@@ -52,14 +52,14 @@ if ( ! function_exists( 'understrap_posted_on' ) ) {
 				'understrap_posted_by',
 				sprintf(
 					'<span class="byline"> %1$s<span class="author vcard"> <a class="url fn n" href="%2$s">%3$s</a></span></span>',
-					$posted_on ? esc_html_x( 'by', 'post author', 'understrap' ) : esc_html_x( 'Posted by', 'post author', 'understrap' ),
+					$posted_on ? esc_html_x( 'By', 'post author', 'understrap' ) : esc_html_x( 'Posted by', 'post author', 'understrap' ),
 					esc_url( get_author_posts_url( $author_id ) ),
 					esc_html( get_the_author_meta( 'display_name', $author_id ) )
 				)
 			);
 		}
 
-		echo $posted_on . $byline; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo $byline . " • " . $posted_on ; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
 

0f1e53c74c5a21010880cfff9b42311a6c867d94
Emanoel <victoremanoel23@gmail.com>
2024-06-18 20:05:21 +0200

    Change the formatting of the search results page to match the archive

--- search.php
+++ search.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying search results pages
  *
@@ -6,60 +7,63 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 get_header();
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 
 ?>
 
 <div class="wrapper" id="search-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="<?php echo esc_attr($container); ?>" id="content" tabindex="-1">
 
 		<div class="row">
 
 			<?php
 			// Do the left sidebar check and open div#primary.
-			get_template_part( 'global-templates/left-sidebar-check' );
+			get_template_part('global-templates/left-sidebar-check');
 			?>
 
-			<main class="site-main" id="main">
+			<header class="page-header">
+				<h1 class="page-title">
+					<?php
+					printf(
+						/* translators: %s: query term */
+						esc_html__('Search Results for: %s', 'understrap'),
+						'<span>' . get_search_query() . '</span>'
+					);
+					?>
+				</h1>
+			</header><!-- .page-header -->
 
-				<?php if ( have_posts() ) : ?>
+			<!-- To remove the posts-main in case of changing the behaviour back
+			to the original one. -->
+			<main class="site-main posts-main" id="main">
 
-					<header class="page-header">
+				<?php if (have_posts()) : ?>
 
-							<h1 class="page-title">
-								<?php
-								printf(
-									/* translators: %s: query term */
-									esc_html__( 'Search Results for: %s', 'understrap' ),
-									'<span>' . get_search_query() . '</span>'
-								);
-								?>
-							</h1>
-
-					</header><!-- .page-header -->
 
 					<?php /* Start the Loop */ ?>
 					<?php
-					while ( have_posts() ) :
+					while (have_posts()) :
 						the_post();
 
 						/*
 						 * Run the loop for the search to output the results.
 						 * If you want to overload this in a child theme then include a file
 						 * called content-search.php and that will be used instead.
+						 * I've replaced the content-search template by the
+						 * content-posts template to match the behaviour of the archive page
 						 */
-						get_template_part( 'loop-templates/content', 'search' );
+						get_template_part('loop-templates/content', 'posts');
 					endwhile;
 					?>
 
 				<?php else : ?>
 
-					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
+					<?php get_template_part('loop-templates/content', 'none'); ?>
 
 				<?php endif; ?>
 
@@ -70,7 +74,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 			understrap_pagination();
 
 			// Do the right sidebar check and close div#primary.
-			get_template_part( 'global-templates/right-sidebar-check' );
+			// get_template_part( 'global-templates/right-sidebar-check' );
 			?>
 
 		</div><!-- .row -->
